87f7dc63d5d0ef3a94ad596d04290a750c14c7d5
Derrick Cui <derrick_cui@berkeley.edu>
2023-01-21 15:48:23 -0800

    block explorer

--- src/App.js
+++ src/App.js
@@ -21,6 +21,8 @@ const alchemy = new Alchemy(settings);
 
 function App() {
   const [blockNumber, setBlockNumber] = useState();
+  const [block, setBlock] = useState();
+  const [blockTr, setBlockWithTransactions] = useState();
 
   useEffect(() => {
     async function getBlockNumber() {
@@ -28,9 +30,94 @@ function App() {
     }
 
     getBlockNumber();
-  });
+  }, []);
 
-  return <div className="App">Block Number: {blockNumber}</div>;
-}
+  useEffect(() => {
+    async function getBlock() {
+        setBlock(await alchemy.core.getBlock(blockNumber));
+    }
+
+    getBlock();
+  }, [blockNumber]);
+
+  useEffect(() => {
+    async function getBlockWithTransactions() {
+
+        setBlockWithTransactions(await alchemy.core.getBlockWithTransactions(blockNumber));
+
+
+    }
+    getBlockWithTransactions();
+
+  }, [blockNumber]);
+
+
+    return (
+    <div className="App">
+      <p>Block Number: {blockNumber} </p>
+      {block &&<div>
+          <p>Block Hash: {block.hash}</p>
+          <p>Timestamp: {block.timestamp}</p>
+          <p>Nonce: {block.nonce}</p>
+          <p>Parent Hash: {block.parentHash}</p>
+       <p>Miner: {block.miner}</p>
+        </div>}
+
+        {blockTr &&<div>
+          <p>Block Transactions index 0's Hash!: {blockTr.transactions[0].hash}</p>
+          <p>To: {blockTr.transactions[0].to}</p>
+          <p>From: {blockTr.transactions[0].from}</p>
+          {/* <p>Value: {blockTr.transactions[0].val}</p> */}
+          {/* <p>Data: {blockTr.transactions[0].data}</p> */}
+        </div>}
+    </div>
+  );}
+
+
+  
+
+
+
+// function App() {
+//   const [blockNumber, setBlockNumber] = useState();
+
+//   useEffect(() => {
+//     async function getBlockNumber() {
+//       setBlockNumber(await alchemy.core.getBlockNumber());
+//     }
+
+//     getBlockNumber();
+//   });
+
+//   useEffect(() => {
+//     async function getBlock() {
+//       if (blockNumber) {
+//         setBlock(await alchemy.core.getBlock(blockNumber));
+//       }
+//     }
+
+//     getBlock();
+//   }, [blockNumber]);
+
+//   return <div className="App">Block Number: {blockNumber}</div>;
+// }
 
 export default App;
+
+// <div>
+        //   <p>Block Hash: {block.hash}</p>
+        //   <p>Timestamp: {block.timestamp}</p>
+        //   <p>Nonce: {block.nonce}</p>
+        //   <p>Parent Hash: {block.parentHash}</p>
+        //   <p>Miner: {block.miner}</p>
+        //   <p>Extra Data: {block.extraData}</p>
+        //    {/* <p>Gas Limit: {block.gasLimit}</p> */}
+        //   <p>Time Stamp: {block.timestamp}</p> 
+        //   {/* <p>First transaction: {blockTransactions.transactions[0]}</p> */}
+        // </div>
+
+
+      //   Block Hash: {block.hash}
+      // <p>Timestamp: {block.timestamp}</p>
+      //  <p>Nonce: {block.nonce}</p>
+       
